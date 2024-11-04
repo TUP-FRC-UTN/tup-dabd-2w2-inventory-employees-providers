2ab06cc9cd523e8789e2dff7b1f2131bb942b5e2
Gonzalo Correa <113199@tecnicatura.frc.utn.edu.ar>
2024-11-04 18:57:19 -0300

    fix titulos

---
+++ src/app/components/provider/service-list/service-list-info/service-list-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-list-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-list-info.component.html',
+  styleUrl: './service-list-info.component.css'
+})
+export class ServiceListInfoComponent {
+
+}
