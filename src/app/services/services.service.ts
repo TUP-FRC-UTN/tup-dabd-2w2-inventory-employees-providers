cfb4c1a522673165d614b50a5275004ce6a8f9b0
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2024-11-04 06:15:45 -0300

    services service dependencies

---
+++ src/app/services/services.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Service } from '../models/service.model';
+import { PaginatedResponse } from '../models/api-response';
