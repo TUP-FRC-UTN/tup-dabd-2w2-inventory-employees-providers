b9493e0bb7aceeb48eb1c16ea9315bbb8adb71e1
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2024-11-04 06:22:11 -0300

    get page base

--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -11,4 +11,24 @@ export class ServicesService {
     private apiUrl = 'http://localhost:8013/service'; //URL de la API para servicios
     
     constructor(private http: HttpClient) {}
+
+    getServices(filters?: {
+        name?: string,
+        type?: string,
+        enabled?: boolean,
+        page?: number,
+        size?: number
+      }): Observable<PaginatedResponse<Service>> {
+        let params = new HttpParams();
+      
+        if (filters) {
+          Object.keys(filters).forEach(key => {
+            const value = filters[key as keyof typeof filters];
+            if (value !== undefined && value !== '') {
+              params = params.append(key, value.toString());
+            }
+          });
+        }
+        return this.http.get<PaginatedResponse<Service>>(`${this.apiUrl}/pageable`, { params });
+      }
 }
