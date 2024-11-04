c1ea30b80f910ef9f0c6c29f0ddc2001e9fa27ee
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2024-11-04 08:30:40 -0300

    service-list dependencies

--- src/app/components/provider/service-list/service-list.component.ts
+++ src/app/components/provider/service-list/service-list.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, inject, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { NgbModal, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { Router, RouterModule } from '@angular/router';
+import { ServicesService } from '../../../services/services.service';
+import { Service } from '../../../models/service.model';
+import { ToastService, MainContainerComponent, ConfirmAlertComponent } from 'ngx-dabd-grupo01';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-service-list',
