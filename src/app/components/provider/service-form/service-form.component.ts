47f496fedb64d83f31723aedd2de1eb68ac19182
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2024-11-04 07:52:49 -0300

    service form dependencies

--- src/app/components/provider/service-form/service-form.component.ts
+++ src/app/components/provider/service-form/service-form.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ServicesService } from '../../../services/services.service';
+import { Service } from '../../../models/service.model';
+import Swal from 'sweetalert2';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastService } from 'ngx-dabd-grupo01';
 
 @Component({
   selector: 'app-service-form',
@@ -8,5 +15,5 @@ import { Component } from '@angular/core';
   styleUrl: './service-form.component.css'
 })
 export class ServiceFormComponent {
-  
+
 }
