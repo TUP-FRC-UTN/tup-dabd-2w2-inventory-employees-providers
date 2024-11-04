d90f59d3e8ce21474e5f33f1339d59873d8fdc44
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2024-11-04 06:16:31 -0300

    injectable

--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -3,3 +3,10 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Service } from '../models/service.model';
 import { PaginatedResponse } from '../models/api-response';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class ServicesService {
+
+}
