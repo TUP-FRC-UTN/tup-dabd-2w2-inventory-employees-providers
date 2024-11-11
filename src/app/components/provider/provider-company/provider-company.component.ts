27f51417f778cf76159083102e940eacdb654d68
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2024-11-11 02:25:09 -0300

    provider company

---
+++ src/app/components/provider/provider-company/provider-company.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-company',
+  standalone: true,
+  imports: [],
+  templateUrl: './provider-company.component.html',
+  styleUrl: './provider-company.component.css'
+})
+export class ProviderCompanyComponent {
+
+}
