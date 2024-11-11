4925d5dc97edb91407b46ff8e65713b592322955
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2024-11-11 09:33:51 -0300

    service update

---
+++ src/app/components/provider/provider-service-update/provider-service-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-service-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './provider-service-update.component.html',
+  styleUrl: './provider-service-update.component.css'
+})
+export class ProviderServiceUpdateComponent {
+
+}
