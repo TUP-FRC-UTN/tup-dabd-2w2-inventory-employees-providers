c7188859cd41183b35c55f460db61191056936ef
405660-NIETO <405660@tecnicatura.frc.utn.edu.ar>
2024-11-11 00:28:15 -0300

    company service

---
+++ src/app/services/suppliers/company.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Company } from '../../models/suppliers/company.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+
+  private apiCompanyUrl = 'http://localhost:8009/companies';
+
+  constructor(private http: HttpClient) { }
+
+  getCompanies(): Observable<Company[]> {
+    return this.http.get<Company[]>(this.apiCompanyUrl);
+  }
+
+  getCompanyById(id: number): Observable<Company> {
+    return this.http.get<Company>(`${this.apiCompanyUrl}/${id}`);
+  }
+
+  createCompany(company: Company): Observable<Company> {
+    return this.http.post<Company>(this.apiCompanyUrl, company);
+  }
+
+  updateCompany(id: number, updatedCompany: Partial<Company>): Observable<Company> {
+    return this.http.put<Company>(`${this.apiCompanyUrl}/${id}`, updatedCompany);
+  }
+
+  deleteCompany(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiCompanyUrl}/${id}`);
+  }
+}
