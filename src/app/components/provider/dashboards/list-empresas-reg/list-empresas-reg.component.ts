b3a7aca9e3cc842f5b313d674d4121ef3509c920
114094_Di_Bella_Pedro_Agustin <114094@tecnicatura.frc.utn.edu.ar>
2024-11-14 02:15:40 -0300

    reportesModales

---
+++ src/app/components/provider/dashboards/list-empresas-reg/list-empresas-reg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-empresas-reg',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-empresas-reg.component.html',
+  styleUrl: './list-empresas-reg.component.css'
+})
+export class ListEmpresasRegComponent {
+
+}
